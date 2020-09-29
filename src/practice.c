2f977b15164c8c104573ba2f512bbad87ea73904
realcapacitor <shivam.patange.entc.2019@vpkbiet.org>
2020-09-29 19:26:49 +0530

    String Functions Finished

--- src/practice.c
+++ src/practice.c
@@ -136,9 +136,9 @@ int main(){
 	//but modern compilers there's no need to add that null character space
 	printf("%s\n",hi);
 	//A string can also be declared as a array of characters as follows
-	//But here in this case we need to manualy a null character at the end otherwise
+	//But here in this case we need to manualy add a null character at the end otherwise
 	//the compiler will think that the string is not ended and will include the characters
-	//aferthe 'd' in the same string according to its memory location
+	//after the 'd' in the same string according to its memory location
 	char w[6]={'w','o','r','l','d','\0'};
 	printf("%s\t %d %d",w,&w,&hi);
 */
@@ -148,10 +148,11 @@ int main(){
 	//size(length/No. of characters)
 	//of a string. this function doesn't include the null character
 	char hi[50] ="HEllo";
+	//in above example length of the string is 5
 	printf("The length of hi[] is %d\n",strlen(hi));
 	//2.strcat()	strcat() function is used to concat(merge) Two strings
 	//i.e. putting 2nd string at the end of the first one
-	//but thers a condition here the size of first string must be as large as it can
+	//but thers a condition the size of first string must be as large as it can
 	//have both first and second string in it, the result is saved in first string
 	//here we can not leave []s empty
 	char w[]=" World";
@@ -165,7 +166,83 @@ int main(){
 //	printf("%s\n",strlwr (str));
 	//5.strupr()	used to uppercase whole sentence
 	//6.strrev()	used to reverse whole sentence
-	//7.strcmp()	used to compare to strings
+	//7.strcmp()	used to compare to strings returns 0 when they are same
 */
+/*
+//String input
+	//there are 3 string input method(functions)
+	//1.scanf()
+	//2.gets()
+	//3.fgets()
+	////////////////////////////////////////////
+	//1.scanf()		when using scanf() theres no need to add '&' before
+	//varible name
+	//a.%s syntax;
+	char hi[10];
+	//scanf("%s",hi);
+	printf("%s",hi);
+	//The scanf("%s") function scans string until the space is encountered.
+	//so to input a long string with spaces init we use a gets() function
+	//2.gets()
+	char w[20];
+	//gets(w);
+	printf("%s ",w);
+	//but using gets() is not safe as it can scan undefined number of characters
+	//so we use fgets() as a best alternative
+	//fgets()
+	char v[20];
+	fgets(v , 20, stdin);
+	printf("%s",v);
+*/
+/*
+//String output
+	//there are methods of string output
+	//1.printf()
+	//2.puts()
+	//3.fputs()
+//String to Number
+	//converting a string to a number is done by the use of atoi() function
+	//if briefly explaining we can say that atoi() function is converting string to
+	//integer only only those strings which contains a number(By mistake or somthing)
+	//like char hi[10]="69"
+	//int atoi() convrets to int
+	//double atoi() converts to float
+	//long atoi() converts to long int
+	char hi[10];
+	fgets(hi , 20, stdin);
+	int num =atoi(hi);
+	printf("%d",num);
+	*/
+//Array of string
+	//2D array can also be used store a strings
+	// it has syntax as follows
+	//to access the elements of the array we must declare it as a char pointer(*)
+	// char* hi[no of string][max no of chars among all];
+	//							 ex.	  char hi[3][6]={
+	//													"Hello",
+	//													"world",
+	//													"Shivam"
+	//													};
+	char hi[3][7]={
+					"Hello",
+					"world",
+					"Shivam"
+				};
+printf("The strings in the array are :\n");
+	for(int i=0;i<3;i++){
+		printf("%s\n",hi[i]);
+	}
+
+	//we can use other way by just leaving []s empty like below example
+	char* w[]={
+	"hello",
+	"world",
+	"Shivam"
+	};
+	puts("Leaving square brackets empty");
+	for(int j=0;j<3;j++){
+		printf("%s\t",w[j]);
+	}
 	return 0;
+
 }
